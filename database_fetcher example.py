689bcec0c92ee22e3b7141e7719d7fef02e81abb
Paul Pelletier <paul.alain.pelletier@gmail.com>
2024-11-23 17:47:54 +0100

    name change for examples

--- database_fetcher example.py
+++ database_fetcher example.py
@@ -18,17 +18,21 @@ connection_string = (
 fetcher = DatabaseFetcher(connection_string, use_sqlalchemy=False)
 
 # Define a SQL query
-query = """
+query = ["""
 SELECT TOP (100) [QUOTE_UNIXTIME], [QUOTE_READTIME], [QUOTE_DATE]
 FROM [DataMining].[dbo].[OptionData]
-"""
+""",
+"""SELECT TOP (100) [QUOTE_UNIXTIME], [QUOTE_READTIME], [QUOTE_DATE]
+FROM [DataMining].[dbo].[OptionData]
+"""]
 
 # Fetch data
 try:
     start_time = time.time()
-    data = fetcher.fetch(query)
+    data_0 = fetcher.fetch(query[0])
+    data_1 = fetcher.fetch(query[1])
     execution_time = time.time() - start_time
-    print(data.head())
-    print(f"Query executed in {execution_time:.4f} seconds")
+    print(data_0.head())
+    print(f"Queries executed in {execution_time:.4f} seconds")
 except ValueError as e:
     print(f"Error: {e}")
