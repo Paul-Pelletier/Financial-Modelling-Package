1b7d59b64da8786e604984107b82d9e9ab391d50
Paul Pelletier <paul.alain.pelletier@gmail.com>
2024-11-25 22:40:46 +0100

    Raw IV example modification

--- IVRawIndicators_example.py
+++ IVRawIndicators_example.py
@@ -4,18 +4,19 @@ import matplotlib.pyplot as plt
 
 data = pd.read_csv('raw_IV_data.csv', sep = ";")
 print(data.columns)
-for expiry in pd.unique(data["YTE"]):
-    first_smile = data[data["YTE"] == expiry][["Moneyness", "C_IV", "P_IV"]]
-    print(first_smile)
-    moneyness = first_smile['Moneyness']
-    call_implied_volatility = first_smile['C_IV']
-    put_implied_volatility = first_smile['P_IV']
-    plt.scatter(moneyness, put_implied_volatility, label = "puts")
-    plt.scatter(moneyness, call_implied_volatility, label = "calls")
-    plt.legend()
-    plt.grid()
-    plt.show()
-
+first_smile = data[data["YTE"] == pd.unique(data["YTE"])[0]][["Moneyness", "C_IV", "P_IV"]]
+print(first_smile)
+moneyness = first_smile['Moneyness']
+call_implied_volatility = first_smile['C_IV']
+put_implied_volatility = first_smile['P_IV']
 indicators = ImpliedVolatilitySmileIndicators(moneyness, call_implied_volatility, put_implied_volatility)
-attributes = vars(indicators)
-print(attributes)
+plt.scatter(moneyness, put_implied_volatility, label = "puts")
+plt.scatter(moneyness, call_implied_volatility, label = "calls")
+plt.scatter(moneyness, indicators.call_minus_put_iv, label = "indicator")
+plt.legend()
+plt.grid()
+plt.show()
+
+#indicators = ImpliedVolatilitySmileIndicators(moneyness, call_implied_volatility, put_implied_volatility)
+#attributes = vars(indicators)
+#print(attributes)
