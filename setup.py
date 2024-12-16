a555eebb72f45139826665a9aa986a62c2d1ced5
Paul Pelletier <paul.alain.pelletier@gmail.com>
2024-12-16 23:14:37 +0100

    package restructuring

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup, find_packages
 
-with open("requirements.txt") as f:
-    required = f.read().splitlines()
-
 setup(
     name="financial_modelling",
     version="0.1.0",
     description="A package for financial modelling pipelines.",
-    author="Paul Pelletier",
-    packages=find_packages(),
-    install_requires=required,  # Read dependencies from requirements.txt
+    author="Your Name",
+    packages=find_packages(),  # Automatically finds 'financial_modelling' and submodules
+    install_requires=[
+        "pandas",
+        "numpy",
+        "scipy"
+    ],
     python_requires=">=3.8",
 )
