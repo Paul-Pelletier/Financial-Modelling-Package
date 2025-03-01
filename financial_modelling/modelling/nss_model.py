0312fab33d81891ef24ce731e68832c51a898cb7
Paul Pelletier <paul.alain.pelletier@gmail.com>
2025-03-02 00:47:44 +0100

    testing with ns model

--- financial_modelling/modelling/nss_model.py
+++ financial_modelling/modelling/nss_model.py
@@ -15,33 +15,30 @@ class NelsonSiegelSvensson:
     - lambda2: Decay factor for the second hump
     """
     
-    def __init__(self, beta0=2000, beta1=2000, beta2=2000, beta3=2000, lambda1=5, lambda2=15):
+    def __init__(self, beta0=2000, beta1=2000, beta2=2000, lambda1=0.1):
         self.beta0 = beta0
         self.beta1 = beta1
         self.beta2 = beta2
-        self.beta3 = beta3
         self.lambda1 = lambda1
-        self.lambda2 = lambda2
         self.observed_rates = None
         self.observed_maturities = None
 
     @staticmethod
-    def forward_curve(t, beta0, beta1, beta2, beta3, lambda1, lambda2):
+    def forward_curve(t, beta0, beta1, beta2, lambda1):
         """
         Nelson-Siegel-Svensson forward rate function.
         """
         term1 = beta0
-        term2 = beta1 * np.exp(-t / lambda1)
-        term3 = beta2 * (t / lambda1) * np.exp(-t / lambda1)
-        term4 = beta3 * (t / lambda2) * np.exp(-t / lambda2)
-        return term1 + term2 + term3 + term4
+        term2 = beta1 * np.exp(t / lambda1)
+        term3 = beta2 * (t / lambda1) * np.exp(t / lambda1)
+        return term1 + term2 + term3
 
     def fit(self, maturities, forward_rates):
         """
         Fit the NSS model to observed forward rates.
         """
         # Initial guess
-        initial_params = [self.beta0, self.beta1, self.beta2, self.beta3, self.lambda1, self.lambda2]
+        initial_params = [self.beta0, self.beta1, self.beta2, self.lambda1]
 
 
         self.observed_maturities = maturities
@@ -50,14 +47,14 @@ class NelsonSiegelSvensson:
         params, _ = opt.curve_fit(self.forward_curve, maturities, forward_rates, p0=initial_params, maxfev=10000)
 
         # Update parameters
-        self.beta0, self.beta1, self.beta2, self.beta3, self.lambda1, self.lambda2 = params
+        self.beta0, self.beta1, self.beta2, self.lambda1 = params
         return params
 
     def predict(self, maturities):
         """
         Predict forward rates for given maturities.
         """
-        return self.forward_curve(maturities, self.beta0, self.beta1, self.beta2, self.beta3, self.lambda1, self.lambda2)
+        return self.forward_curve(maturities, self.beta0, self.beta1, self.beta2, self.lambda1)
 
     def plot(self, maturities, observed_rates=None):
         """
@@ -69,8 +66,8 @@ class NelsonSiegelSvensson:
         plt.plot(maturities, predicted_rates, label="Fitted NSS Curve", linewidth=2)
         if observed_rates is not None:
             plt.scatter(self.observed_maturities, self.observed_rates, color='red', label="Observed Data")
-        plt.axvline(x=self.lambda1, color='red', linestyle="--", label=f"1st Hump (λ1={self.lambda1:.2f})")
-        plt.axvline(x=self.lambda2, color='blue', linestyle="--", label=f"2nd Hump (λ2={self.lambda2:.2f})")
+        #plt.axvline(x=self.lambda1, color='red', linestyle="--", label=f"1st Hump (λ1={self.lambda1:.2f})")
+        #plt.axvline(x=self.lambda2, color='blue', linestyle="--", label=f"2nd Hump (λ2={self.lambda2:.2f})")
         plt.xlabel("Maturity (years)")
         plt.ylabel("Term Structure")
         plt.title("Nelson-Siegel-Svensson parameterization")
--- financial_modelling/pipeline/nss_params.py
+++ financial_modelling/pipeline/nss_params.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 file_path = "E:\\ForwardComputations\\FittedData\\2021\\04\\forward_computation_1617287520.csv"
+file_path = "E:\\ForwardComputations\\FittedData\\2021\\04\\forward_computation_1618933500.csv"
 file = pd.read_csv(file_path, sep = ",")
 file = file[-np.isinf(file['FORWARD'])]
 print(file)
@@ -11,8 +12,6 @@ maturities = (1/(3600*24*365))*(file["EXPIRE_UNIX"]-file["QUOTE_UNIXTIME"])
 forward_rates = file["FORWARD"]
 nss = NelsonSiegelSvensson()
 nss.fit(maturities, forward_rates)
-npmaturities = np.linspace(0, 3, 100)
-plt.plot(maturities, forward_rates, label="Observed")
-plt.show()
+print(nss.beta0, nss.beta1, nss.beta2, nss.lambda1)
 predicted_rates = nss.predict(maturities)
-nss.plot(npmaturities, forward_rates)
+nss.plot(maturities, forward_rates)
