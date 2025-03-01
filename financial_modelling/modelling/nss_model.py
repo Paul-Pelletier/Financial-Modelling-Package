5ebe2a1546d04af61d478cd9ec5d3d9e6a853968
Paul Pelletier <paul.alain.pelletier@gmail.com>
2025-03-02 00:33:28 +0100

    testing

--- financial_modelling/modelling/nss_model.py
+++ financial_modelling/modelling/nss_model.py
@@ -15,13 +15,15 @@ class NelsonSiegelSvensson:
     - lambda2: Decay factor for the second hump
     """
     
-    def __init__(self, beta0=0.02, beta1=-0.03, beta2=0.04, beta3=-0.02, lambda1=5, lambda2=15):
+    def __init__(self, beta0=2000, beta1=2000, beta2=2000, beta3=2000, lambda1=5, lambda2=15):
         self.beta0 = beta0
         self.beta1 = beta1
         self.beta2 = beta2
         self.beta3 = beta3
         self.lambda1 = lambda1
         self.lambda2 = lambda2
+        self.observed_rates = None
+        self.observed_maturities = None
 
     @staticmethod
     def forward_curve(t, beta0, beta1, beta2, beta3, lambda1, lambda2):
@@ -41,6 +43,9 @@ class NelsonSiegelSvensson:
         # Initial guess
         initial_params = [self.beta0, self.beta1, self.beta2, self.beta3, self.lambda1, self.lambda2]
 
+
+        self.observed_maturities = maturities
+        self.observed_rates = forward_rates
         # Fit using scipy.optimize.curve_fit
         params, _ = opt.curve_fit(self.forward_curve, maturities, forward_rates, p0=initial_params, maxfev=10000)
 
@@ -63,7 +68,7 @@ class NelsonSiegelSvensson:
         plt.figure(figsize=(10, 5))
         plt.plot(maturities, predicted_rates, label="Fitted NSS Curve", linewidth=2)
         if observed_rates is not None:
-            plt.scatter(maturities, observed_rates, color='red', label="Observed Data")
+            plt.scatter(self.observed_maturities, self.observed_rates, color='red', label="Observed Data")
         plt.axvline(x=self.lambda1, color='red', linestyle="--", label=f"1st Hump (λ1={self.lambda1:.2f})")
         plt.axvline(x=self.lambda2, color='blue', linestyle="--", label=f"2nd Hump (λ2={self.lambda2:.2f})")
         plt.xlabel("Maturity (years)")
--- financial_modelling/pipeline/nss_params.py
+++ financial_modelling/pipeline/nss_params.py
@@ -1,1 +1,18 @@
-from financial_modelling.modelling.nss_model import NSSParams
+from financial_modelling.modelling.nss_model import NelsonSiegelSvensson
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+file_path = "E:\\ForwardComputations\\FittedData\\2021\\04\\forward_computation_1617287520.csv"
+file = pd.read_csv(file_path, sep = ",")
+file = file[-np.isinf(file['FORWARD'])]
+print(file)
+maturities = (1/(3600*24*365))*(file["EXPIRE_UNIX"]-file["QUOTE_UNIXTIME"])
+forward_rates = file["FORWARD"]
+nss = NelsonSiegelSvensson()
+nss.fit(maturities, forward_rates)
+npmaturities = np.linspace(0, 3, 100)
+plt.plot(maturities, forward_rates, label="Observed")
+plt.show()
+predicted_rates = nss.predict(maturities)
+nss.plot(npmaturities, forward_rates)
